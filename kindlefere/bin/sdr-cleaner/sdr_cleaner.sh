df19c3f1195392721e0663d8c1ce349b20ace271
Rachel Tang <291054446@qq.com>
2017-09-23 10:37:04 +0800

    Fixed bug of Python
    which causes the plugin to be unable to work

--- kindlefere/bin/sdr-cleaner/sdr_cleaner.sh
+++ kindlefere/bin/sdr-cleaner/sdr_cleaner.sh
@@ -5,6 +5,11 @@
 #------------------------
 
 KINDLE_PATH=/chroot/mnt/us
+if [[ $PATH != *"/mnt/us/python/bin"* ]]
+    then
+        export PATH=$PATH:/mnt/us/python/bin
+fi
+alias python="python2.7"
 
 # sdr_cleaner
 cd ${KINDLE_PATH}/extensions/kindlefere/bin/sdr-cleaner
