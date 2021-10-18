89c9528e4ae25a7540bfeec003a8a280f266632f
bookfere <bookfere@gmail.com>
2021-10-19 00:17:01 +0800

    Check local python if no symbolic link in bin.

--- bookfere-tools/bin/sdr-cleaner/sdr_cleaner.sh
+++ bookfere-tools/bin/sdr-cleaner/sdr_cleaner.sh
@@ -6,21 +6,24 @@
 
 . ./bin/config.sh
 
-PYTHON=/usr/bin/python
-PYTHON3=/usr/bin/python3
+SUCCESS=0
+PATH1=/usr/bin/python3
+PATH2=/mnt/us/python3/bin/python3.8
+PATH3=/usr/bin/python
+PATH4=/mnt/us/python2/bin/python2.7
 
-# Check if python installed.
-if [ -f $PYTHON3 ]; then
-    $PYTHON3 $SDR_SCRIPT $KINDLE_PATH
-elif [ -f $PYTHON ]; then
-    $PYTHON2 $SDR_SCRIPT $KINDLE_PATH
-else
-    $MSG_VIEWER 'Need install python to Kindle.' string
-    exit 0
-fi
+for PYTHON in $PATH1 $PATH2 $PATH3 $PATH4; do
+    if [ -f $PYTHON ]; then
+        $PYTHON $SDR_SCRIPT
+        SUCCESS=1
+        break
+    fi
+done
 
 if [ $? = 0 ]; then
-    $MSG_VIEWER 'SDR folder clear completed.' string
+    $MSG_VIEWER 'All unused SDR folders are cleared.' string
+elif [ $SUCCESS = 0 ]; then
+    $MSG_VIEWER 'Need to install python on Kindle.' string
 else
-    $MSG_VIEWER 'Unexpected error.' string
+    $MSG_VIEWER 'Unexpected error occurs.' string
 fi
